396ae58f7c52cd31cd13696a68a676d35d71f31a
Muhammed Ahmed <ahmedkhatri1998@gmail.com>
2022-03-26 22:05:47 +0500

    Sign up feature initial commit

---
+++ KFCClone/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KFCClone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private DbContext _context;
+        public AuthController(DbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(string username, string password)
+        {
+            if (username == "admin" && password == "admin")
+            {
+                return Ok(new { token = "admin" });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
