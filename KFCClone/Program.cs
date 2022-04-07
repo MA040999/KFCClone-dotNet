d7256202a7589dbe819c08e09da4cfe7855b71ef
Muhammed Ahmed <ahmedkhatri1998@gmail.com>
2022-04-08 03:43:46 +0500

    Fetched categories and displayed on FE. Also login screen UI done.

--- KFCClone/Program.cs
+++ KFCClone/Program.cs
@@ -59,4 +59,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "Category",
+    pattern: "{controller=Category}/{action=Index}/{categoryId}");
+
 app.Run();
